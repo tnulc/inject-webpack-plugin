cba83d620cf5cfbbc7ff94377a3352a376b444ad
Christopher Lunt <github@kirisu.co.uk>
2017-03-09 20:35:06 +0000

    v0.4.0

--- index.js
+++ index.js
@@ -13,30 +13,38 @@ InjectWebpackPlugin.prototype.apply = function(compiler) {
   var _this = this;
   this.dependencyMap = {};
 
-  this.filesToBeReplaced = Object.keys(this.options).map(function (file) {
+  this.filesToBeReplaced = Object.keys(this.options).map(function(file) {
     if (_this.nodeModules.indexOf(file) !== -1) {
-      _this.dependencyMap[file] = path.resolve(compiler.context, _this.options[file]);
+      _this.dependencyMap[file] = path.resolve(
+        compiler.context,
+        _this.options[file]
+      );
       return file;
     }
 
     var resolvedPath = path.resolve(compiler.context, file);
-    _this.dependencyMap[resolvedPath] = path.resolve(compiler.context, _this.options[file]);
+    _this.dependencyMap[resolvedPath] = path.resolve(
+      compiler.context,
+      _this.options[file]
+    );
     return resolvedPath;
   });
 
-  compiler.plugin('normal-module-factory', function (nmf) {
+  compiler.plugin('normal-module-factory', function(nmf) {
     nmf.plugin('before-resolve', function(module, callback) {
-      if(module.context.includes(compiler.context)) {
-        var nonNodeModuleIndex = _this.filesToBeReplaced.indexOf(path.resolve(module.context, module.request));
+      if (module.context.includes(compiler.context)) {
+        var nonNodeModuleIndex = _this.filesToBeReplaced.indexOf(
+          path.resolve(module.context, module.request)
+        );
         var nodeModuleIndex = _this.filesToBeReplaced.indexOf(module.request);
 
-        if(nonNodeModuleIndex !== -1 || nodeModuleIndex !== -1) {
-          var newFile = nonNodeModuleIndex !== -1 ? path.resolve(module.context, module.request) : module.request;
+        if (nonNodeModuleIndex !== -1 || nodeModuleIndex !== -1) {
+          var newFile = nonNodeModuleIndex !== -1
+            ? path.resolve(module.context, module.request)
+            : module.request;
           module.request = _this.dependencyMap[newFile];
-          module.dependency.request = _this.dependencyMap[newFile];
-          module.dependency.userRequest = _this.dependencyMap[newFile];
         }
-      };
+      }
 
       return callback(null, module);
     });
